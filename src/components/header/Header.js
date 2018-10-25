c38698929893184852bba6a35b4939aa5be29099
Laurens <laurens.trotsenburg@hotmail.com>
2018-10-25 15:55:46 +0200

    Scss for Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,9 +9,9 @@ class Header extends Component{
                 </div>
                 <nav className='nav'>
                     <ul className='navigtion'>
-                        <li className='navigation__list'><Link to='/'>Home</Link></li>
-                        <li className='navigation__list'><Link to='/Projects'>Projects</Link></li>
-                        <li className='navigation__list'><Link to='/About'>About</Link></li>
+                        <li className='navigation__list'><Link className='navigation__link' to='/'>Home</Link></li>
+                        <li className='navigation__list'><Link className='navigation__link' to='/Projects'>Projects</Link></li>
+                        <li className='navigation__list'><Link className='navigation__link' to='/About'>About</Link></li>
                     </ul>
                 </nav>
             </header>
