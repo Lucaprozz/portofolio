a200ad38a00c5b96ede448f22a97e08b1209ab03
Lucaprozz <1038179@idcollege.nl>
2018-10-25 09:13:48 +0200

    added scss

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Hier komt de routing ah niffauww</h1>
+            <h2>ik ga ook ff de provider maken zometeen</h2>
+        </div>
+    );
+  }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './app/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js'
@@ -9,13 +9,13 @@ module.exports = {
     module: {
         rules: [
             { test: /\.(js)$/, use: 'babel-loader'},
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader']},
+            { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader']},
         ]
     },
     mode: 'development',
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'app/index.html'
+            template: './public/index.html'
         })
     ]
 }
