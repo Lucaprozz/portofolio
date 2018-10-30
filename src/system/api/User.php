17a9222ca6f52cceedb9b34b955f10217061d037
Laurens <laurens.trotsenburg@hotmail.com>
2018-10-30 09:18:15 +0100

    Changed data

--- src/system/api/User.php
+++ src/system/api/User.php
@@ -1,6 +1,6 @@
 <?
 // Headers
-header('Acces-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 include_once '../config/Database.php';
