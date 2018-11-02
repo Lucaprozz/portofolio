9ef74b2e396eaaf5a086ee75cb18d379f9344054
Laurens21 <laurens.trotsenburg@hotmail.com>
2018-11-02 12:57:51 +0100

    cc

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../components/header/Header'
+import Canvas from '../components/canvas/Canvas'
 
 class Home extends Component{
     render(){
         return(
             <>
-                < Header />
-                
+                <Header />
+                <Canvas />
             </>
         )
     }
