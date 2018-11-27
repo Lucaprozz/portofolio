13d8c517395cd9c0472a7a1543a6f6e2f20b0572
Lucaprozz <1038179@idcollege.nl>
2018-11-27 15:20:17 +0100

    updated styles folder

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './content.scss'
+class Content extends Component{
+    render(){
+        return(
+            <footer className='footer'>
+                
+            </footer>
+        )
+    }
+}
+
+export default Content;
