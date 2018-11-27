17d7340bb1b310279dfe08cf3e8ce4423940afdf
Laurens <1018265@idcollege.nl>
2018-11-27 14:41:38 +0100

    added

---
+++ src/components/content/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './content.scss'
+class Content extends Component{
+    render(){
+        return(
+            <section>
+                <div>
+                    <img src='' alt='Profile-image'/>
+                </div>
+                <div>
+
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Content;
