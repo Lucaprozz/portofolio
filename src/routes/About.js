381a58f59376bc9010186b36ad8f6302dc235755
Laurens <1018265@idcollege.nl>
2018-11-27 14:51:51 +0100

    added

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Header from '../components/header/Header'
+import Header from '../components/header'
 
 class About extends Component{
     render(){
