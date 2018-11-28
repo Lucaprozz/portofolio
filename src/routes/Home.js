1114e5b60b00c05a7d5c16b074ace2a0c6675780
Laurens <1018265@idcollege.nl>
2018-11-28 08:58:36 +0100

    Fixed bug

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 import Header from '../components/header'
-import Content from '../components/content'
 class Home extends Component{
     render(){
         return(
             <>
                 <Header />
-                <Content />
             </>
         )
     }
