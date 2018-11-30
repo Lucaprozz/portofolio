86dc92f6d8d8ef07708162df3a1dd2b0c8e56a1c
Lucaprozz <1038179@idcollege.nl>
2018-11-30 09:25:11 +0100

    working tree doet gaar

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-import './content.scss'
+import './Footer.scss'
+
 class Content extends Component{
     render(){
         return(
             <footer className='footer'>
-                
+				<div className='footer__flex'>
+					<h1 className='footer__title'>Links</h1>
+					<h1 className='footer__title'>Links</h1>
+					<h1 className='footer__title'>Links</h1>    
+				</div>
+                <div className='footer__flex'>
+                    <p className='footer__para'>flkwefgbwjbkwbegkvwrefbjebvwervbwjrfknkwbew fwerfgwerfgwerg</p>
+					<p className='footer__para'>Links</p>
+					<p className='footer__para'>Links</p> 
+                </div>
             </footer>
         )
     }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../components/header'
+import Footer from '../components/footer'
 
 class Projects extends Component{
     render(){
         return(
             <>
                 < Header />
+                <Footer />
             </>
         )
     }
