a7eb2eaaee8c5c7a7ea09120e4bf69f68dcb5d73
Laurens <1018265@idcollege.nl>
2018-11-30 09:30:07 +0100

    Fixed merge conflict

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react'
 import Header from '../components/header'
-<<<<<<< HEAD
 import Footer from '../components/footer'
-=======
-import Slider from '../components/slider'
->>>>>>> 4e1b2c1e0fc1d9efb7b3651ef3a191e7f573d0c4
+import Slider from '../components/slider';
 class Home extends Component{
     render(){
         return(
             <>
                 <Header />
-<<<<<<< HEAD
-                <Footer />
-=======
                 <Slider />
->>>>>>> 4e1b2c1e0fc1d9efb7b3651ef3a191e7f573d0c4
+                <Footer />
             </>
         )
     }
